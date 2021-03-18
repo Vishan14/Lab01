0a4620da60906ddb5b255a8acec96486df04dab7
Hp <Hp@LAPTOP-HD8KI8FA>
2021-03-18 22:32:07 +0800

    exe7

---
+++ Lab1/src/Lab1/Exercise7.java
@@ -0,0 +1,23 @@
+
+package Lab1;
+import java.util.Scanner;
+
+public class Exercise7 {
+
+	public static void main(String[] args)
+    {
+        double weight;
+        double weightonMars;
+        
+        Scanner sc = new Scanner(System.in);
+        
+        System.out.print("Enter weight(kg): ");
+        
+        weight = sc.nextDouble();
+        weightonMars = (weight/9.81)*3.711;
+
+         System.out.println("Weight on Earth: " +weight + "kg");
+         System.out.println("Weight on Mars: " +String.format("%.2f", weightonMars ) + "kg");
+
+    }
+}
